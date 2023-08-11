d66f282fd28e171bdc7a0a32217eefd181766c93
irsad <irsadkures@gmail.com>
2023-08-12 02:23:46 +0300

    Java Training Note

--- A _ Level/Unit_2 (variables, data types and basic operators)/Main.java
+++ A _ Level/Unit_2 (variables, data types and basic operators)/Main.java
@@ -13,6 +13,7 @@ public class Main {
 
     */ 
   }
+} 
 //-------------------------------------------------------------------------------------------------------------
 //                                                                     int -- float -- char -- boolean --string
 
@@ -151,17 +152,32 @@ public class Main {
     }
 }
 //-------------------------------------------------------------------------------------------------------------
+//                                                                                             float --- double
+public class Main {
+    public static void main(String[] args) {
+        // Double : 64 bit - 8 byte
+        // Float : 32 bit - 4 byte     
+        // Otomatik Dönüştürme :  int --> float ---> double       
+       double sayi1 = 70.25;
+       double sayi2 = 60d;
+       double sayi3 = 80.2;
+       
+       float a = 70.25f;
+       float b = 60f;
+       float c = 80.2f;
+        System.out.println("Ortalama: " + (sayi1 + sayi2 + sayi3) / 3 );
+        System.out.println("Ortalama2: " + (a + b + c) / 3 );      
+    }
+}
+
+//-------------------------------------------------------------------------------------------------------------
 //                                                                                 byte -- short -- int -- long
 public class Main {
     public static void main(String[] args) {
         int a = 10;
-        
         int b = a * 2;
-        
         int c = a * b * 2;
-        
         int toplam = a + b + c;
-        
         toplam = toplam - 5;
         System.out.println("Toplam = " + toplam);          
     }
@@ -185,3 +201,135 @@ public class Main {
 
   }
 }  
+//-------------------------------------------------------------------------------------------------------------
+//                                                                                               int and string
+public class Main {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 20;
+    int z = x + y;
+    System.out.println(z);
+
+    String x = "10";
+    String y = "20";
+    String z = x + y;  // z will be 1020 (a String)
+
+    String x = "10";
+    int y = 20;
+    String z = x + y;  // z will be 1020 (a String)
+  }
+}
+//-------------------------------------------------------------------------------------------------------------
+//                                                                                                  add strings
+public class Main {
+  public static void main(String args[]) {
+    String firstName = "irşad";
+    String lastName = "küreş";
+    System.out.println(firstName + " " + lastName);
+
+
+    String firstName = "irşad ";
+    String lastName = "küreş";
+    System.out.println(firstName.concat(lastName));
+
+    String firstName = "irşad ";
+    String lastName = "küreş";
+    String fullName = firstName + lastName;
+    System.out.println(fullName); 
+
+  }
+}
+//-------------------------------------------------------------------------------------------------------------
+//                                                                                                       string
+public class Main {
+  public static void main(String[] args) {
+    String greeting = "Hello";
+    System.out.println(greeting);
+
+    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    System.out.println("The length of the txt string is: " + txt.length());
+
+    String txt = "Hello World";
+    System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+    System.out.println(txt.toLowerCase());   // Outputs "hello world"
+    
+    String txt = "Please locate where 'locate' occurs!";
+    System.out.println(txt.indexOf("locate")); // Outputs 7   bir dizgede (boşluk dahil) belirtilen bir metnin ilk geçtiği yerin dizininiindexOf() (konumu) döndürür :
+  }
+}
+//-------------------------------------------------------------------------------------------------------------
+//                                                                                 Strings - Special Characters
+Escape character	       Result	          Description
+      \'	                 '	            Single quote
+      \"	                 "	            Double quote
+      \\	                 \	            Backslash
+
+public class Main {
+  public static void main(String[] args) {
+    String txt = "We are the so-called \"Vikings\" from the north.";
+    System.out.println(txt);
+    String txt = "It\'s alright.";
+    System.out.println(txt);
+    String txt = "The character \\ is called backslash.";
+    System.out.println(txt);
+  }
+}
+//-------------------------------------------------------------------------------------------------------------
+//                                                                                 Strings - Special Characters
+Code	      Result	
+\n	        New Line	
+\r	        Carriage Return	
+\t	        Tab	
+\b	        Backspace	
+\f	        Form Feed
+
+public class Main {
+  public static void main(String[] args) {
+    String txt = "Hello\nWorld!";
+    System.out.println(txt);
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    String txt = "Hello\rWorld!";
+    System.out.println(txt);
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    String txt = "Hello\tWorld!";
+    System.out.println(txt);
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    String txt = "Hel\blo World!";
+    System.out.println(txt);
+  }
+}
+
+//-------------------------------------------------------------------------------------------------------------
+//                                                                                                  Operatörler
+public class Main {
+    public static void main(String[] args) {
+        /*
+        + ---> Toplama Operatörü
+        - ---> Çıkarma Operatörü
+        / ---> Bölme Operatörü
+        * ---> Çarpma Operatörü
+        % ---> Kalan Operatörü
+
+        */
+        
+        System.out.println(3 + 4.2);
+        System.out.println(3 - 5f);
+        System.out.println(10 / 4.0);
+        System.out.println(3 * 4.5);
+        System.out.println(10 % 4);
+        
+        System.out.println((3f / 4) + (4 * 5));              
+    }
+}
