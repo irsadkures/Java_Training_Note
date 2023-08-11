04cd7ec044571d67383640d11164447ae02e6691
irsad <irsadkures@gmail.com>
2023-08-11 17:03:44 +0300

    Java Training Note

--- A _ Level/Unit_2 (variables, data types and basic operators)/Main.java
+++ A _ Level/Unit_2 (variables, data types and basic operators)/Main.java
@@ -166,3 +166,22 @@ public class Main {
         System.out.println("Toplam = " + toplam);          
     }
 }
+
+//-------------------------------------------------------------------------------------------------------------
+//                                                                                                char - string
+public class Main {
+  public static void main(String[] args) {
+
+    char myGrade = 'B';
+    System.out.println(myGrade);
+
+    char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+    System.out.println(myVar1);
+    System.out.println(myVar2);
+    System.out.println(myVar3);
+
+    String greeting = "Hello World";
+    System.out.println(greeting);
+
+  }
+}  
